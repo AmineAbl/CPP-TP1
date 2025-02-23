265a7d0b28273025abe704980e2038a978d8a063
AmineAbl <amine.aboulaiche20@gmail.com>
2025-02-23 12:47:19 +0000

    second commit

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -2,20 +2,28 @@
 
 using namespace std;
 
-int main()
-{
-    int tab[6] = {3,8,5,2,4,7};
-    int position = 0;
+int main() {
+    int tab[6] = {3, 8, 5, 2, 4, 7};
+    int* debut = tab;
+    int* ptr = tab;
 
-    for (int i = 0; i < 6; i++) {
-        if (tab[i] % 2 == 0) {
-            swap(tab[i], tab[position]);
-            position++;
+    for(int i=0;i<6;i++){
+        if(*ptr % 2 == 0){
+            int tmp=*ptr;
+            *ptr=*debut;
+            *debut=tmp;
+            debut++;
+
         }
+        ptr++;
     }
+
+
+    cout << "le nouveau tableau est : ";
     for (int i = 0; i < 6; i++) {
-        cout <<tab[i];
+        cout << tab[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
