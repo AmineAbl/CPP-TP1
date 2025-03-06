f2de550b646ba796f1427095d3013a0a2d2bb864
AmineAbl <amine.aboulaiche20@gmail.com>
2025-03-06 22:50:09 +0000

    first

--- ex4/main.cpp
+++ ex4/main.cpp
@@ -2,24 +2,46 @@
 
 using namespace std;
 
+int Kadan(int t[3][4],int n,int m){
+
+int max_totale=0;
+int pos=0;
+
+for(int i=0;i<n ; i++){
+    int max_ending_here=0;
+    int max_so_far=0;
+        for(int j=0;j<m ; j++){
+
+        max_ending_here=max_ending_here+t[i][j];
+        max_ending_here=max(max_ending_here,0);
+
+        max_so_far=max(max_so_far,max_ending_here);
+
+    }
+    if(max_totale < max_so_far){
+        pos=i;
+    }
+    max_totale=max(max_totale,max_so_far);
+}
+cout<< "sous tableau maximale [";
+for(int j=0; j<m ; j++){
+    cout<< "\t"<<t[pos][j];
+}
+cout<<"]"<<endl;
+
+
+return max_totale;
+}
+
 int main()
 {
-    int tab[3][4] = {
-        {-2, 1, -3, 4},
-        {-1, 2, 1, -5},
-        {4, -1, 2, 1}
+    int t[3][4] ={
+    {-2,1,-3,4},
+    {1,2,1,-5},
+    {4,-1,2,1}
     };
-    int maxs = -10000;
-    for (int i = 0; i < 3; i++) {
-        int s = 0;
-        for (int j = 0; j < 4; j++) {
-            s += tab[i][j];
-        }
-        if (s > maxs) {
-            maxs = s;
-        }
-    }
-    cout << "la plus grande somme de ligne est : " << maxs << endl;
 
+    int k=Kadan(t,3,4);
+    cout<<"la somme est : " << k << endl;
     return 0;
 }
--- ex5/main.cpp
+++ ex5/main.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int main() {
-    int matrice[3][3] = {
+int main()
+{
+    int t[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
-    int tab[9];
-    int index = 0;
+
+    bool tab[10] = {false};
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            tab[index++] = matrice[i][j];
-        }
-    }
-    sort(tab, tab + 9);
-
-    bool estPermutation = true;
-    for (int i = 1; i < 9; i++) {
-        if (tab[i] != tab[i-1]) {
-            estPermutation = true;
-        } else {
-            estPermutation = false;
-            break;
+            if (tab[t[i][j]] == false) {
+                tab[t[i][j]] = true;
+            }
+            else {
+                cout<<"pas une permutation " <<endl;
+                return 0;
+            }
         }
     }
 
-    if (estPermutation) {
-        cout << "C'est une permutation." << "\n";
-    } else {
-        cout << "Ce n'est pas une permutation." << "\n";
+    for (int i = 1; i < 10; i++) {
+        if (tab[i] == false) {
+            cout << "pas une permutation" << endl;
+            return 0;
+        }
     }
 
+    cout << "est une permutation" << endl;
     return 0;
 }
